cbaed45b650ed3f2c2597f6523065262e1084238
Rafael MMM <75507122+Rafael-Mota-Moraes@users.noreply.github.com>
2021-02-03 19:42:06 -0300

    Add files via upload

---
+++ Transitions/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Meu site</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<div class="box">	
+	</div><!-- box -->
+
+	<!--
+	<form>
+		<input type="text" name="">
+	</form>
+	-->
+
+</body>
+</html>
